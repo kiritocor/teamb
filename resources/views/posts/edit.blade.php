bd5330ee0db354b42d5a0a19a04dcd34451b1975
c0b211447a <c0b211447a@edu.teu.ac.jp>
2023-10-22 16:58:27 +0900

    コントローラーとえでぃっと

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+        <meta name="csrf-token" content="{{ csrf_token() }}"/>
         <title>Blog</title>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -30,20 +31,15 @@
     <body>
         <h1 class="title">編集画面</h1>
         <div class="content">
-             <div class='content__title'>
-                    <h2>タイトル</h2>
-                    <h2>{{ $post->title }}</h2>
-                </div>
-                <div class='content__body'>
-                    <h2>本文</h2>
-                    <h2>{{ $post->body }}</h2>
-                    
-                </div>
-            
+            <h2>タイトル</h2>
+            <div class='content__title'><h2>{{ $post->title }}</h2></div>
+            <h2>本文</h2>
+            <div class='content__body'><h2>{{ $post->body }}</h2></div>
              <button id="replaceButton1">Trick1</button>
              <button id="replaceButton2">Trick2</button>
              <button id="replaceButton3">Trick3</button>
              <button id="replaceButton4">Trick4</button>
+             <button id="sendButton">save</button>
         </div>
     </body>
     <script>
@@ -117,5 +113,35 @@
             }
         });
     </script>
-    
+    <script>
+        document.getElementById("sendButton").addEventListener("click", function () {
+            const csrfToken = document.head.querySelector('meta[name="csrf-token"]').content;
+            const trickBodies = document.querySelectorAll(".content__body");
+            const trickTitles = document.querySelectorAll(".content__title");
+            const messages = []; 
+            trickTitles.forEach(
+                trickTitle => { messages.push(trickTitle.innerHTML);}
+            );
+            trickBodies.forEach(
+                trickBody => { messages.push(trickBody.innerHTML);}
+            ); 
+            messages.push(`{{ $post->id }}`);
+            
+            console.log(messages);
+            const jsonData = JSON.stringify(messages); 
+            console.log(jsonData);
+                                    
+            fetch(`/posts/{{ $post->id }}/saveData`, {
+                method: 'POST',
+                headers: {
+                    'X-CSRF-TOKEN': csrfToken,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(
+                //送信するやつ
+                messages
+                )
+            }).then(res => {}).catch(err => console.log(err));
+        });
+    </script>
 </html>
